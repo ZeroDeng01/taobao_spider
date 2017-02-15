feb4f377b4107c8010f12966853bf236e7a455e4
ZeroDeng <denglin0105@vip.qq.com>
2017-02-15 23:22:59 +0800

    no commit message

---
+++ python2.7引用包/Python27/Scripts/pywin32_testall.py
@@ -0,0 +1,4 @@
+#!C:\Python27\python.exe
+# EASY-INSTALL-SCRIPT: 'pypiwin32==219',u'pywin32_testall.py'
+__requires__ = 'pypiwin32==219'
+__import__('pkg_resources').run_script('pypiwin32==219', u'pywin32_testall.py')
